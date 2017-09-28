a70144380d94f89a40b2fc3225e78da0c0191077
Andrew Kirilenko <iced@gojuno.com>
2017-09-28 13:05:00 +0300

    README and setting version to 0.1.0.

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name="jm",
-    version="0.1",
+    version="0.1.0",
     author="Andrew Kirilenko",
     author_email="iced@gojuno.com",
     maintainer="Andrew Kirilenko",
