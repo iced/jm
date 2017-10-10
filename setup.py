5ea1e29f5da5369ecc1d42dd9e21df3917bde034
Andrew Kirilenko <iced@gojuno.com>
2017-10-10 15:02:45 +0300

    Bumping to 0.1.1.

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name="jm",
-    version="0.1.0",
+    version="0.1.1",
     author="Andrew Kirilenko",
     author_email="iced@gojuno.com",
     maintainer="Andrew Kirilenko",
