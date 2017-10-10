42be7dc7898667b8aae777ccfcee408e1a45fe7f
Andrew Kirilenko <iced@gojuno.com>
2017-10-10 14:49:22 +0300

    Support for any type of geometry in fit_source.

--- jm/js/map_fit_source.js
+++ jm/js/map_fit_source.js
@@ -1,22 +1,27 @@
 requirejs(["mapboxgl"], function(mapboxgl) {
     var map = window.__jm_maps["{{uuid}}"];
 
+    var findBounds = function(bounds, coordinates) {
+        if (coordinates[0] instanceof Array) {
+            coordinates.forEach(function(c) {
+                bounds = findBounds(bounds, c);
+            });
+        } else {
+            if (bounds === undefined) {
+                bounds = new mapboxgl.LngLatBounds(coordinates, coordinates);
+            } else {
+                bounds = bounds.extend(coordinates);
+            }
+        }
+        return bounds;
+    }
+
     var run = function() {
         var source = map.getSource("{{source_uuid}}");
         var geojson = source._data;
-        var coordinates = geojson.features[0].geometry.coordinates[0];
-        if (coordinates[0] instanceof Array) {
-            coordinates = coordinates[0];
-        }
-        var bounds = new mapboxgl.LngLatBounds(coordinates, coordinates);
+        var bounds = undefined;
         geojson.features.forEach(function(feature) {
-            coordinates = feature.geometry.coordinates;
-            if (coordinates[0] instanceof Array) {
-                coordinates = coordinates[0];
-            }
-            bounds = coordinates.reduce(function(bounds, coord) {
-                return bounds.extend(coord);
-            }, bounds);
+            bounds = findBounds(bounds, feature.geometry.coordinates);
         });
 
         map.fitBounds(bounds, {
